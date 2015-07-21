4dd297828583d706588d75071aace885864d94f6
Daniel Santana <daniel@everymundo.com>
2015-07-21 17:13:35 -0400

    BUG3 - NODEJS: Server allows to save all fields in blank

--- models/movies.server.model.js
+++ models/movies.server.model.js
@@ -2,9 +2,9 @@ var mongoose     = require( 'mongoose' );
 
 var MoviesSchema = new mongoose.Schema({
     title:        {type: String, required: false},
-    releaseYear:  {type: String, required: true },
-    director:     {type: String, required: true },
-    genre:        {type: String, required: true },
+    releaseYear:  {type: String, required: false },
+    director:     {type: String, required: false },
+    genre:        {type: String, required: false },
     created_on:   {type: Date  , required: true, default: Date.now},
 });
 
